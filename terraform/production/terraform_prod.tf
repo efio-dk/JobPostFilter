1152e743473a13c5e4b517227e66d1d83d97b810
Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
2020-02-05 14:21:21 +0100

    Production release (#7)
    * Publishing repeated job post to a new queue rather than ignoring it
    
    * Changed JobPost model to include job post url. Added functionality to verify if url already exists and publish job post to different queues
    
    * Added Json schema validation, adapted the code to use new json schema.
    
    * Refactored string to int in json schema
    
    * Publish to error sqs queue when job post has invalid format
    
    * Refactored implementation into several classes, took helper methods and put them into Utility class, made test for them.
    
    * Creating IDB and IQueue Facades - added their implementations (AWSDB and AWSQueue). Moved the implementation from the Function.cs to those newly created files.
    
    * Added more tests for GetQueueForMessage functionality. Added Code Coverage and Moq NuGet  packages
    
    * Adding terraform config for staging env
    
    * Ignoring code coverage files
    
    * Creating a dedicated terraform folder with staging tf file
    
    * Updating read and write capacities due to the billing_mode set by default to PROVISIONED, therefore those fields are required.
    
    * Terraform production config. Adding a default aws region. Set up terraform to use S3 bucket to keep the terraform state
    
    Co-authored-by: David Alves <cph-dd69@cphbusiness.dk>

---
+++ terraform/production/terraform_prod.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+
+resource "aws_sqs_queue" "prod-existing-job-post-queue" {
+  name                      = "prod_ExistingJobPosts"
+
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "prod-invalid-job-post-queue" {
+  name                      = "prod_InvalidJobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "prod-job-post-queue" {
+  name                      = "prod_JobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "prod-processed-job-post-queue" {
+  name                      = "prod_ProcessedJobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+
+resource "aws_dynamodb_table" "prod-post-body-hashes-table" {
+  name           = "prod_PostBodyHashes"
+  hash_key       = "sourceHash"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "sourceHash"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "prod-post-body-hashes-table"
+    Environment = "staging"
+  }
+}
+
+resource "aws_dynamodb_table" "prod-post-url-hashes-table" {
+  name           = "prod_PostUrlHashes"
+  hash_key       = "urlHash"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "urlHash"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "prod-post-url-hashes-table"
+    Environment = "staging"
+  }
+}
---
+++ terraform/staging/terraform_stg.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+
+resource "aws_sqs_queue" "stg-existing-job-post-queue" {
+  name                      = "stg_ExistingJobPosts"
+
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "stg-invalid-job-post-queue" {
+  name                      = "stg_InvalidJobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "stg-job-post-queue" {
+  name                      = "stg_JobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_sqs_queue" "stg-processed-job-post-queue" {
+  name                      = "stg_ProcessedJobPosts"
+  
+  tags = {
+    Environment = "staging"
+  }
+}
+
+
+resource "aws_dynamodb_table" "stg-post-body-hashes-table" {
+  name           = "stg_PostBodyHashes"
+  hash_key       = "sourceHash"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "sourceHash"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "stg-post-body-hashes-table"
+    Environment = "staging"
+  }
+}
+
+resource "aws_dynamodb_table" "stg-post-url-hashes-table" {
+  name           = "stg_PostUrlHashes"
+  hash_key       = "urlHash"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "urlHash"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "stg-post-url-hashes-table"
+    Environment = "staging"
+  }
+}
