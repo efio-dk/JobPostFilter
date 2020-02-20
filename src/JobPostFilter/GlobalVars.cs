34cc1284c29db54c786e3f87b6dc524ec27ac9d3
Elitsa <elit0451@edu.eal.dk>
2020-02-20 16:41:38 +0100

    Added config to be able to run redis in production

--- src/JobPostFilter/GlobalVars.cs
+++ src/JobPostFilter/GlobalVars.cs
@@ -9,6 +9,7 @@ namespace JobPostFilter
             public static readonly string INVALID_QUEUE = "stg_InvalidJobPosts";
             public static readonly string BODY_TABLE = "stg_PostBodyHashes";
             public static readonly string URL_TABLE = "stg_PostUrl";
+            public static readonly string REDIS_ENDPOINT = "stg-job-post-cluster.ovby8n.0001.euw1.cache.amazonaws.com";
         #else
         // release stuff goes here
             public static readonly string EXISTING_QUEUE = "prod_ExistingJobPosts";
@@ -16,6 +17,7 @@ namespace JobPostFilter
             public static readonly string INVALID_QUEUE = "prod_InvalidJobPosts";
             public static readonly string BODY_TABLE = "prod_PostBodyHashes";
             public static readonly string URL_TABLE = "prod_PostUrl";
+            public static readonly string REDIS_ENDPOINT = "prod-job-post-cluster.ovby8n.0001.euw1.cache.amazonaws.com";
         #endif
     }
 }
